41f2fa65f3522787c0efdfb8dd3de7827b817c9a
nandal <sagarnandal52@gmail.com>
2022-12-29 22:57:56 +0530

    Message that describes what this change does

---
+++ routes/patient.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+   patientRegister,
+   patientCreateReport,
+   patientAllReports,
+} = require("../controller/patientController");
+
+
+const {
+   registerDoctor,
+   loginDoctor,
+   profileDoctor,
+} = require("../controller/doctorController");
+
+const { verifyToken } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+// router.use(verifyToken);
+
+router.post("/register", patientRegister);
+
+// patient id
+router.post("/:id/create_report", patientCreateReport);
+
+// patient id
+router.get("/:id/all_reports", patientAllReports);
+
+module.exports = router;
